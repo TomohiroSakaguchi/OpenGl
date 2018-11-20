f9c885cfb17f36146203e8e8933cb67081b83634
TomohiroSakaguchi <stomohiro59@gmail.com>
2018-11-20 14:00:22 +0900

    グラデーションになりました

--- prog1.c
+++ prog1.c
@@ -3,11 +3,14 @@
 void display(void)
 {
   glClear(GL_COLOR_BUFFER_BIT);
-  glColor3d(1.0, 0.0, 0.0);
   glBegin(GL_POLYGON);
+  glColor3d(1.0, 0.0, 0.0); /* 赤 */
   glVertex2d(-0.9, -0.9);
+  glColor3d(0.0, 1.0, 0.0); /* 緑 */
   glVertex2d(0.9, -0.9);
+  glColor3d(0.0, 0.0, 1.0); /* 青 */
   glVertex2d(0.9, 0.9);
+  glColor3d(1.0, 1.0, 0.0); /* 黄 */
   glVertex2d(-0.9, 0.9);
   glEnd();
   glFlush();
